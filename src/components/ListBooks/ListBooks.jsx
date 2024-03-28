7cef5b875b735285cb082cc114bc5f91fdc154ca
takbirgazi <takbirgazibd@gmail.com>
2024-03-28 21:15:44 +0600

    Almost Done

--- src/components/ListBooks/ListBooks.jsx
+++ src/components/ListBooks/ListBooks.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import ReadBookList from "../ReadBookList/ReadBookList";
 import WishListBook from "../WishListBook/WishListBook";
 import { getBook } from "../../utility/storeData";
@@ -7,12 +7,30 @@ import { getWishBook } from "../../utility/wishlistStore";
 
 const ListBooks = () => {
     const allBook = useLoaderData();
+    const [sortReadBook, setSortReadBook] = useState([]);
     const [currentReadBook ,setCurrentReadBook] =  useState([]);
+
+    const readBookFilterHanllrbyRate = ()=>{
+        const sampleData = currentReadBook.sort((a, b) => (b.rating - a.rating))
+        setSortReadBook(sampleData);
+    }
+
+    const readBookFilterHanllrbypgNo = ()=>{
+        const sampleData = currentReadBook.sort((a, b) => (b.totalPages - a.totalPages))
+        setSortReadBook(sampleData);
+    }
+
+    const readBookFilterHanllrbyyear = ()=>{
+        const sampleData = currentReadBook.sort((a, b) => (b.yearOfPublishing - a.yearOfPublishing))
+        setSortReadBook(sampleData);
+    }
     useEffect(()=>{
         const storedBook = getBook();
         if(allBook.length > 0){
             const readBooks = allBook.filter( book => storedBook.includes(book.id));
             setCurrentReadBook(readBooks);
+            setSortReadBook(currentReadBook);
+
         }
 
     },[allBook])
@@ -38,13 +56,12 @@ const ListBooks = () => {
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn m-1">Click</div>
                         <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li className="my-2"><NavLink>Rating</NavLink></li>
-                            <li className="my-2"><NavLink>Number of pages</NavLink></li>
-                            <li className="my-2"><NavLink>Publisher year</NavLink></li>
+                            <li className="my-2" onClick={readBookFilterHanllrbyRate}><a>Rating</a></li>
+                            <li className="my-2" onClick={readBookFilterHanllrbypgNo}><a>Number of pages</a></li>
+                            <li className="my-2" onClick={readBookFilterHanllrbyyear}><a>Publisher year</a></li>
                         </ul>
                     </div>
                 </div>
-
             <div>
 
                 <div role="tablist" className="tabs tabs-lifted">
@@ -52,7 +69,7 @@ const ListBooks = () => {
                     <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" checked />
                     <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
                         <div className="flex flex-col gap-5">
-                            {currentReadBook.map( book => <ReadBookList key={book.id} bookInfo={book}></ReadBookList>)}       
+                            {sortReadBook.map( book => <ReadBookList key={book.id} bookInfo={book}></ReadBookList>)}       
                         </div>
                     </div>
 
